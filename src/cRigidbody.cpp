81f95142a6ea7dc8419a2627d00743330a6784c9
gelis07 <baggelispoz@gmail.com>
2025-09-05 17:03:41 +0300

    fixed clipped points

--- src/cRigidbody.cpp
+++ src/cRigidbody.cpp
@@ -464,13 +464,14 @@ std::vector<glm::vec3> cRigidBody::GetCollisionPoints(const std::vector<glm::vec
         refNorm *= -1.0f;
 
     float max = glm::dot(refNorm, ref.max);
-    if (glm::dot(refNorm, cp2[0]) - max < 0.0f) {
-        cp2.erase(cp2.begin());
+    std::vector<glm::vec3> points;
+    if (!(glm::dot(refNorm, cp2[0]) - max < 0.0f)) {
+        points.push_back(cp2[0]);
     }
-    if (glm::dot(refNorm, cp2[1]) - max < 0.0f) {
-        cp2.erase(cp2.begin() + 1);
+    if (!(glm::dot(refNorm, cp2[1]) - max < 0.0f)) {
+        points.push_back(cp2[1]);
     }
-    return cp2;
+    return points;
 }
 std::vector<glm::vec3> cRigidBody::Clip(glm::vec3 v1, glm::vec3 v2, glm::vec3 n, float o)
 {
